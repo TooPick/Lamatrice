0e121bc8d5061b588b2402e65fc68dc4806657c7
Maëva Lauzier <maeva.lauzier@gmail.com>
2016-11-21 00:04:44 +0100

    Base de données

---
+++ src/AppBundle/Entity/Cart.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cart
+ *
+ * @ORM\Table(name="cart")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartRepository")
+ */
+class Cart
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="validated", type="boolean")
+     */
+    private $validated;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="UserBundle\Entity\User", inversedBy="carts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\CartProduct", mappedBy="cart")
+     */
+    private $cartProducts;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set validated
+     *
+     * @param boolean $validated
+     *
+     * @return Cart
+     */
+    public function setValidated($validated)
+    {
+        $this->validated = $validated;
+
+        return $this;
+    }
+
+    /**
+     * Get validated
+     *
+     * @return bool
+     */
+    public function getValidated()
+    {
+        return $this->validated;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \UserBundle\Entity\User $user
+     *
+     * @return Cart
+     */
+    public function setUser(\UserBundle\Entity\User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \UserBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->cartProducts = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add cartProduct
+     *
+     * @param \AppBundle\Entity\CartProduct $cartProduct
+     *
+     * @return Cart
+     */
+    public function addCartProduct(\AppBundle\Entity\CartProduct $cartProduct)
+    {
+        $this->cartProducts[] = $cartProduct;
+
+        return $this;
+    }
+
+    /**
+     * Remove cartProduct
+     *
+     * @param \AppBundle\Entity\CartProduct $cartProduct
+     */
+    public function removeCartProduct(\AppBundle\Entity\CartProduct $cartProduct)
+    {
+        $this->cartProducts->removeElement($cartProduct);
+    }
+
+    /**
+     * Get cartProducts
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCartProducts()
+    {
+        return $this->cartProducts;
+    }
+}
---
+++ src/AppBundle/Entity/CartProduct.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CartProduct
+ *
+ * @ORM\Table(name="cart_product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartProductRepository")
+ */
+class CartProduct
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Cart", inversedBy="cartProducts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $cart;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Product")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $product;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return CartProduct
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set cart
+     *
+     * @param \AppBundle\Entity\Cart $cart
+     *
+     * @return CartProduct
+     */
+    public function setCart(\AppBundle\Entity\Cart $cart)
+    {
+        $this->cart = $cart;
+
+        return $this;
+    }
+
+    /**
+     * Get cart
+     *
+     * @return \AppBundle\Entity\Cart
+     */
+    public function getCart()
+    {
+        return $this->cart;
+    }
+
+    /**
+     * Set product
+     *
+     * @param \AppBundle\Entity\Product $product
+     *
+     * @return CartProduct
+     */
+    public function setProduct(\AppBundle\Entity\Product $product)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return \AppBundle\Entity\Product
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+}
