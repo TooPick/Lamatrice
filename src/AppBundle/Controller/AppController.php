380d1fb6050d22be877ee9f9bfa01022c6e00c6b
MaevaL <lauzier.maeva@hotmail.fr>
2016-12-19 13:09:53 +0100

    début theme et accueil

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -3,11 +3,31 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Enum\CategoryTypeEnum;
 
 class AppController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AppBundle:App:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+    	$productRepository = $em->getRepository("AppBundle:Product");
+    	
+    	$productsStationery = $productRepository->findBy(array(
+    		"category" => CategoryTypeEnum::TYPE_STATIONERY,
+    	), null, 10);
+    	
+    	$productsPlastic = $productRepository->findBy(array(
+    		"category" => CategoryTypeEnum::TYPE_PLASTIC,
+    	), null, 10);
+
+        return $this->render('AppBundle:App:index.html.twig', array(
+        	'productsStationery' => $productsStationery,
+        	'productsPlastic' => $productsPlastic,
+        ));
+    }
+
+    public function testAction()
+    {
+    	return $this->render('AppBundle:App:base.html.twig');
     }
 }
