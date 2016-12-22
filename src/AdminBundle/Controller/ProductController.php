97140377cae04f2b22a7e59cc2aec6df89f645cf
BaptisteBlouin <blouinbaptiste@gmail.com>
2016-12-22 10:03:02 +0100

    Reorga adminProduct, et rajoute layout admin

---
+++ src/AdminBundle/Controller/ProductController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Product controller.
+ *
+ */
+class ProductController extends Controller
+{
+    /**
+     * Lists all product entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository('AppBundle:Product')->findAll();
+
+        return $this->render('AdminBundle:Admin:product/index.html.twig', array(
+            'products' => $products,
+        ));
+    }
+    
+    /**
+     * Lists all product stock entities.
+     */
+    public function stockAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+    'SELECT p
+    FROM AppBundle:Product p
+    WHERE p.quantity > :quantity
+    ORDER BY p.quantity ASC'
+)->setParameter('quantity', '0');
+        $products = $query->getResult();
+
+        return $this->render('AdminBundle:Admin:product/stock.html.twig', array(
+            'products' => $products,
+        ));
+    }
+
+    /**
+     * Creates a new product entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm('AdminBundle\Form\ProductType', $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush($product);
+
+            return $this->redirectToRoute('admin_product_show', array('id' => $product->getId()));
+        }
+
+        return $this->render('AdminBundle:Admin:product/new.html.twig', array(
+            'product' => $product,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a product entity.
+     *
+     */
+    public function showAction(Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+
+        return $this->render('AdminBundle:Admin:product/show.html.twig', array(
+            'product' => $product,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing product entity.
+     *
+     */
+    public function editAction(Request $request, Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+        $editForm = $this->createForm('AdminBundle\Form\ProductType', $product,array(
+     'attr' => array('stock' => false)));
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_product_index');
+        }
+
+        return $this->render('AdminBundle:Admin:product/edit.html.twig', array(
+            'product' => $product,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    public function editStockAction(Request $request, Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+        $editForm = $this->createForm('AdminBundle\Form\ProductType', $product,array(
+     'attr' => array('stock' => true)));
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_product_stock');
+        }
+
+        return $this->render('AdminBundle:Admin:product/edit.html.twig', array(
+            'product' => $product,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a product entity.
+     *
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+        $form = $this->createDeleteForm($product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush($product);
+        }
+
+        return $this->redirectToRoute('admin_product_index');
+    }
+
+    /**
+     * Creates a form to delete a product entity.
+     *
+     * @param Product $product The product entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Product $product)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_product_delete', array('id' => $product->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
